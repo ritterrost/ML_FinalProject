79eb991e4bbbb8c4df2bac1b55535232b7d1042b
tillrostalski <mr453@uni-heidelberg.de>
2022-03-07 21:33:03 +0100

    formated code using black

--- bomberman_rl/agent_code/alex_agent/event_functions.py
+++ bomberman_rl/agent_code/alex_agent/event_functions.py
@@ -1,39 +1,52 @@
+"""
+  Input: self, events, old_game_state, new_game_state
+  Output: Amount of reward: int
+"""
+
 import numpy as np
 import events as e
+
 
 def walked_towards_closest_coin(self, events, old_game_state, new_game_state):
     old_pos, new_pos = old_game_state["self"][-1], new_game_state["self"][-1]
     old_arena = old_game_state["field"]
 
-    #surrounding walls
-    wall_above = old_arena[old_pos[0], old_pos[1]+1] == -1
-    wall_below = old_arena[old_pos[0], old_pos[1]-1] == -1
-    wall_right = old_arena[old_pos[0]+1, old_pos[1]] == -1
-    wall_left = old_arena[old_pos[0]-1, old_pos[1]] == -1
+    # surrounding walls
+    wall_above = old_arena[old_pos[0], old_pos[1] + 1] == -1
+    wall_below = old_arena[old_pos[0], old_pos[1] - 1] == -1
+    wall_right = old_arena[old_pos[0] + 1, old_pos[1]] == -1
+    wall_left = old_arena[old_pos[0] - 1, old_pos[1]] == -1
 
-    #position of coins
+    # position of coins
     old_coins, new_coins = old_game_state["coins"], new_game_state["coins"]
-    if len(old_coins)==0:
+    if len(old_coins) == 0:
         return 0
-    elif len(new_coins)==0:
+    elif len(new_coins) == 0:
         return 0
     else:
         old_coin_xy, new_coin_xy = np.asarray(old_coins), np.asarray(new_coins)
-        #old closest coin
+        # old closest coin
         old_closest_index = np.argmin(np.linalg.norm(old_coin_xy - old_pos, axis=1))
         old_coin_pos = old_coin_xy[old_closest_index]
-        #direction of coin relative to old position
+        # direction of coin relative to old position
         coin_direction = old_coin_pos - old_pos
-        #exclude case where player has to move laterally
-        lateral_movement_necessary = (np.all(coin_direction == [0,1]) and wall_above) or (np.all(coin_direction == [0,-1]) and wall_below)\
-                                   or (np.all(coin_direction == [1,0]) and wall_right) or (np.all(coin_direction == [-1,0]) and wall_left)
-        if lateral_movement_necessary: return 0.5
-        #check if there still is a coin at position of old closest coin
+        # exclude case where player has to move laterally
+        lateral_movement_necessary = (
+            (np.all(coin_direction == [0, 1]) and wall_above)
+            or (np.all(coin_direction == [0, -1]) and wall_below)
+            or (np.all(coin_direction == [1, 0]) and wall_right)
+            or (np.all(coin_direction == [-1, 0]) and wall_left)
+        )
+        if lateral_movement_necessary:
+            return 0.5
+        # check if there still is a coin at position of old closest coin
         if e.COIN_COLLECTED in events:
             return 1
         if old_coin_pos in new_coin_xy:
-            #give reward if walked in direction, punish if walked away
-            diff = np.linalg.norm(old_coin_pos-new_pos)-np.linalg.norm(old_coin_pos-old_pos)
-            return np.sign(-diff) #+/-1 after 1 step
+            # give reward if walked in direction, punish if walked away
+            diff = np.linalg.norm(old_coin_pos - new_pos) - np.linalg.norm(
+                old_coin_pos - old_pos
+            )
+            return np.sign(-diff)  # +/-1 after 1 step
         else:
             return 0
--- bomberman_rl/agent_code/alex_agent/train.py
+++ bomberman_rl/agent_code/alex_agent/train.py
@@ -1,11 +1,13 @@
-from agent_code.alex_agent.callbacks import A_TO_NUM, Q_func
 from collections import namedtuple, deque
 import pickle
+from subprocess import call
 from typing import List
-from agent_code.alex_agent.event_functions import walked_towards_closest_coin
-import events as e
-from agent_code.alex_agent.callbacks import state_to_features
 import numpy as np
+import events as e
+
+from agent_code.alex_agent import event_functions
+from agent_code.alex_agent import feature_functions
+from agent_code.alex_agent import callbacks
 
 # This is only an example!
 Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
@@ -14,7 +16,7 @@ Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"
 TRANSITION_HISTORY_SIZE = 1  # keep only ... last transitions
 RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
 COIN_K = 1
-#ALPHA = 0.1
+# ALPHA = 0.1
 BATCH_SIZE = 2000
 GAMMA = 0.1
 
@@ -24,22 +26,25 @@ WALKED_TOWARDS_CLOSEST_COIN = "WALKED_TOWARDS_CLOSEST_COIN"
 WALKED_AWAY_FROM_CLOSEST_COIN = "WALKED_AWAY_FROM_CLOSEST_COIN"
 LATERAL_MOVEMENT = "LATERAL_MOVEMENT"
 
-#for convenience
+# for convenience
 A_NUM = 6
 FEAT_DIM = 6
 
-def response_func(self, gamma = GAMMA):
+
+def response_func(self, gamma=GAMMA):
     transition = self.transitions[-1]
-    next_q_value = Q_func(self, feat=transition.next_state)
+    next_q_value = callbacks.Q_func(self, feat=transition.next_state)
     return transition.reward + gamma * np.max(next_q_value)
 
 
 def forest_update(self):
-    #select random batch of transitions and updates all actions at once
+    # select random batch of transitions and updates all actions at once
     for idx in np.arange(A_NUM):
-        #batch for action denoted by idx
-        if len(self.feat_history[idx])>0:
-            selection_mask = np.random.choice(np.arange(len(self.feat_history[idx])), size = BATCH_SIZE)
+        # batch for action denoted by idx
+        if len(self.feat_history[idx]) > 0:
+            selection_mask = np.random.choice(
+                np.arange(len(self.feat_history[idx])), size=BATCH_SIZE
+            )
             X = np.array(self.feat_history[idx])[selection_mask]
             y = np.array(self.target_history[idx])[selection_mask]
             self.forests[idx].fit(X, y)
@@ -55,49 +60,64 @@ def game_events_occurred(
     self_action: str,
     new_game_state: dict,
     events: List[str],
-    ):
+):
 
     # custom events use event_functions here
     if old_game_state is not None:
-        if walked_towards_closest_coin(self, events, old_game_state, new_game_state) == 1:
+        if (
+            event_functions.walked_towards_closest_coin(
+                self, events, old_game_state, new_game_state
+            )
+            == 1
+        ):
             events.append(WALKED_TOWARDS_CLOSEST_COIN)
-        if walked_towards_closest_coin(self, events, old_game_state, new_game_state) == -1:
+        if (
+            event_functions.walked_towards_closest_coin(
+                self, events, old_game_state, new_game_state
+            )
+            == -1
+        ):
             events.append(WALKED_AWAY_FROM_CLOSEST_COIN)
-        if walked_towards_closest_coin(self, events, old_game_state, new_game_state) == 0.5:
+        if (
+            event_functions.walked_towards_closest_coin(
+                self, events, old_game_state, new_game_state
+            )
+            == 0.5
+        ):
             events.append(LATERAL_MOVEMENT)
 
         self.transitions.append(
             Transition(
-                state_to_features(old_game_state),
+                feature_functions.state_to_features_coin_collector(old_game_state),
                 self_action,
-                state_to_features(new_game_state),
-                #reward_from_events(self, events),
-                total_rewards(self, events, old_game_state, new_game_state)
+                feature_functions.state_to_features_coin_collector(new_game_state),
+                # reward_from_events(self, events),
+                total_rewards(self, events, old_game_state, new_game_state),
             )
         )
-        
-        feat = state_to_features(old_game_state)
+
+        feat = feature_functions.state_to_features_coin_collector(old_game_state)
         Y_tt = response_func(self)
-        idx = A_TO_NUM[self_action]
+        idx = callbacks.A_TO_NUM[self_action]
 
         self.target_history[idx].append(Y_tt)
         self.feat_history[idx].append(feat)
 
-        #self.logger.info(f"self.target_history: {self.target_history}")
-        #self.logger.info(f"self.feat_history: {self.feat_history}")
+        # self.logger.info(f"self.target_history: {self.target_history}")
+        # self.logger.info(f"self.feat_history: {self.feat_history}")
 
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
     self.transitions.append(
         Transition(
-            state_to_features(last_game_state),
+            feature_functions.state_to_features_coin_collector(last_game_state),
             last_action,
             None,
             reward_from_events(self, events),
         )
     )
 
-    #update forests for all actions
+    # update forests for all actions
     forest_update(self)
 
     # Store the model
@@ -116,12 +136,12 @@ def reward_from_events(self, events: List[str]):
         WALKED_TOWARDS_CLOSEST_COIN: 1,
         WALKED_AWAY_FROM_CLOSEST_COIN: -1,
         LATERAL_MOVEMENT: -1,
-        #e.WAITED: -2,
-        #e.INVALID_ACTION: -5,
+        # e.WAITED: -2,
+        # e.INVALID_ACTION: -5,
         e.KILLED_SELF: -100,
-        #e.SURVIVED_ROUND: 100,
+        # e.SURVIVED_ROUND: 100,
         e.CRATE_DESTROYED: 2,
-        #e.BOMB_DROPPED: 2
+        # e.BOMB_DROPPED: 2
         # e.KILLED_OPPONENT: 2,
         # PLACEHOLDER_EVENT: -0.1,  # idea: the custom event is bad
     }
@@ -129,5 +149,5 @@ def reward_from_events(self, events: List[str]):
     for event in events:
         if event in game_rewards:
             reward_sum += game_rewards[event]
-    #self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    # self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
     return reward_sum
