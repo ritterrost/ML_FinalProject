0fc99c5f2a1d3a62351bffefcc60ffe1f757a3af
tillrostalski <mr453@uni-heidelberg.de>
2022-03-24 21:00:41 +0100

    guter main_agent und work_agent

---
+++ bomberman_rl/agent_code/q_table_agent/callbacks.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+from collections import deque
+from .feature_functions import state_to_features, FEAT_DIM
+
+# Game Parameter
+ACTIONS = ["UP", "RIGHT", "DOWN", "LEFT", "WAIT", "BOMB"]
+A_TO_NUM = {"UP": 0, "RIGHT": 1, "DOWN": 2, "LEFT": 3, "WAIT": 4, "BOMB": 5}
+A_IDX = np.arange(0, 6, 1, dtype="int")
+
+#hyperparameters
+MAX_DEPTH = None #better: less than 15
+MAX_LEAF_NODES = 1000
+MIN_SAMPLES_SPLIT = 5
+N_ESTIMATORS = 20
+HISTORY_SIZE = 10000
+EPSILON_TRAIN = 0.2
+RHO_TRAIN = 50
+
+
+def Q_func(self, features):
+    Q_values = []
+
+    for Qs in self.Q_dicts:
+        if tuple(features) in Qs.keys():
+            Q_values.append(Qs[tuple(features)])
+        else:
+            Qs[tuple(features)] = 0
+            Q_values.append(0)
+    print(Q_values)
+    return Q_values
+
+
+def setup(self):
+    self.keep_training = False
+    for arg in sys.argv:
+        if arg=="--keep-training":
+            self.keep_training = True
+            break
+    if self.train:
+
+        self.Q_dicts = [{}, {}, {}, {}, {}, {}]
+        self.Q_values = [0, 0, 0, 0, 0, 0]
+
+        self.feat_history = [deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE)]
+        self.next_feat_history = [deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE)]
+        self.reward_history = [deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE)]
+        self.epsilon = EPSILON_TRAIN
+        self.logger.debug("feat_history", self.feat_history)
+    else:  
+        self.epsilon = 0
+        self.logger.info("Loading model from saved state.")
+        with open("my-saved-model.pt", "rb") as file:
+            self.Q_dicts = pickle.load(file)
+
+
+def policy_alt(self):
+    prob = np.exp(self.Q_pred/self.rho)
+    #sometimes probabilities are NaN
+    mask = np.isnan(prob)
+    prob[mask]=0
+
+    prob = prob/np.sum(prob)
+        
+    return np.random.choice(np.arange(0,6,1), p=prob.reshape(6))
+
+
+def policy(self, Qs):
+    r = np.random.random(1)
+    prob = np.ones(6)/6
+    if r < self.epsilon:
+        return np.random.choice(np.arange(0, 6, 1), p=prob)
+    else:
+        return np.argmax(Qs)
+
+
+def act(self, game_state: dict):
+    feat = state_to_features(game_state)
+    Qs = Q_func(self, feat)
+    self.Q_values = Qs
+    a = policy(self, Qs)
+    self.logger.debug(f"action a in act: {ACTIONS[a]}")
+    return ACTIONS[a]
---
+++ bomberman_rl/agent_code/work_agent/event_functions.py
@@ -0,0 +1,81 @@
+"""
+  Input: self, events, old_game_state, new_game_state
+  Output: Amount of reward: int
+"""
+
+import numpy as np
+import events as e
+
+DROP_BOMB_CLOSE_TO_OTHER = "DROP_BOMB_CLOSE_TO_OTHER"
+WALKED_TO_COIN = "WALKED_TO_COIN"
+DROP_BOMB_NEXT_TO_CRATE = "DROP_BOMB_NEXT_TO_CRATE"
+WALKED_FROM_DANGER = "WALKED_FROM_DANGER"
+STAYS_IN_DANGER_ZONE = "STAYS_IN_DANGER_ZONE"
+HAS_NO_ESCAPE = "HAS_NO_ESCAPE"
+
+
+A_TO_COORD = {
+    "UP": [0, -1],
+    "LEFT": [-1, 0],
+    "RIGHT": [1, 0],
+    "DOWN": [0, 1],
+    "WAIT": [0, 0],
+    "BOMB": [-1, -1],
+}
+
+
+def walked_towards_closest_coin(state, action):
+    if (np.array(A_TO_COORD[action]) == state[0:2]).all(): # align action with direction of coin
+        return 1
+
+
+def walked_from_danger(state, action):
+    if state[8] == 0:  # check if in danger (distance to free tile)
+        return 0
+    else:
+        if (np.array(A_TO_COORD[action]) == state[6:8]).all():  # align action with direction of free field
+            return 1
+        else:
+            return -1
+
+
+def drop_bomb_next_to_crate(state, events):
+    if state[5] == 1:  # distance to crate
+        if e.BOMB_DROPPED in events:
+            return 1
+
+
+def has_no_escape(new_state, action):
+    if action == 'BOMB':
+        if new_state[8] > 4 or new_state[8] == -1:
+            return 1
+
+def drop_bomb_close_to_other(state, action):
+    if state[11] == 1:
+        if action == "BOMB":
+            return 1
+
+
+def reward_from_events(self, events: list[str]):
+    game_rewards = {
+        e.COIN_COLLECTED: 60,
+        e.KILLED_SELF: -100,
+        e.SURVIVED_ROUND: 100,
+        e.GOT_KILLED: -100,
+        e.BOMB_DROPPED: -20,
+        e.KILLED_OPPONENT: 100,
+        e.INVALID_ACTION: -5,
+        DROP_BOMB_NEXT_TO_CRATE: 40,
+        DROP_BOMB_CLOSE_TO_OTHER: 10,
+        WALKED_FROM_DANGER: 40,
+        STAYS_IN_DANGER_ZONE: -40,
+        WALKED_TO_COIN: 40,
+        HAS_NO_ESCAPE: -100,
+    }
+
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
