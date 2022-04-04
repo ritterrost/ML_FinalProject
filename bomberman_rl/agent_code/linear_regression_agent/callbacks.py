6df1d0ee658278701dea210f9a966e3b64549fb1
tillrostalski <mr453@uni-heidelberg.de>
2022-04-04 19:24:58 +0200

    lr to main ´

--- bomberman_rl/agent_code/linear_regression_agent/callbacks.py
+++ bomberman_rl/agent_code/linear_regression_agent/callbacks.py
@@ -1,82 +1,135 @@
-import os
-import sys
-import pickle
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.datasets import make_regression
 from collections import deque
-from .feature_functions import state_to_features, FEAT_DIM
+import os
+import pickle
+import random
 
-# Game Parameter
-ACTIONS = ["UP", "RIGHT", "DOWN", "LEFT", "WAIT", "BOMB"]
+import numpy as np
+
+ACTIONS = ["UP", "RIGHT", "DOWN", "LEFT", "WAIT", "WAIT"]
+SIGHT = 1
+BOMBS_FEAT_SIZE = 12
+COIN_COUNT = 50
+COIN_K = 1
 A_TO_NUM = {"UP": 0, "RIGHT": 1, "DOWN": 2, "LEFT": 3, "WAIT": 4, "BOMB": 5}
-A_IDX = np.arange(0, 6, 1, dtype="int")
+EPSILON = 0.3
 
-#hyperparameters
-MAX_DEPTH = 15
-MAX_LEAF_NODES = 100000
-MIN_SAMPLES_SPLIT = 5
-N_ESTIMATORS = 50
-HISTORY_SIZE = 1000
-EPSILON_TRAIN = 0.1
-RHO_TRAIN = 50
+
+def policy(q_res):
+    r = np.random.random(1)
+    prob = np.array([0.2,0.2,0.2,0.2,0,0.2]) #exclude bombs for now
+    if r < EPSILON:
+        return np.random.choice(np.arange(0,6,1), p=prob)
+    else:
+        return np.argmax(q_res)
+
+
+def Q_func(self, feat):
+    result = []
+    for beta in self.betas:
+        result.append(np.dot(feat, beta))
+    # self.logger.info(f"Q_func result: {result}")
+
+    return result
+
+
+def state_to_features(game_state):
+    # Gather information about the game state
+    arena = game_state["field"]
+    explosion_map = game_state["explosion_map"]
+    coins = game_state["coins"]
+    _, score, bombs_left, (x, y) = game_state["self"]
+    bombs = game_state["bombs"]
+    # not used yet
+    others = [xy for (n, s, b, xy) in game_state["others"]]
+
+    # turn bombs into constant sized feature
+    bomb_xys = [[bomb_x-x, bomb_y-y, bomb_t] for ((bomb_x, bomb_y), bomb_t) in bombs]
+    bomb_xys = np.array(bomb_xys).flatten()
+    num_missing_bombs = BOMBS_FEAT_SIZE - bomb_xys.size
+    bomb_feat = np.concatenate((bomb_xys, np.zeros(num_missing_bombs)))
+
+    ## turn coins into constant sized feature
+    #coins = np.array(coins).flatten()
+    #num_missing_coins = 2 * COIN_COUNT - coins.size
+    #coins_feat = np.concatenate((coins, np.zeros(num_missing_coins)))
+
+    #Alternatively only include the nearest K coins
+    #only give direction of coin (for linear regression)
+    coin_xy_rel = np.array([[coin_x-x,coin_y-y] for (coin_x,coin_y) in coins])
+    if len(coins)<COIN_K:
+        sorted_index = np.argsort(np.linalg.norm(coin_xy_rel, axis=1, ord=1))
+        coins_feat = np.concatenate((np.sign(coin_xy_rel[sorted_index]), np.zeros((COIN_K-len(coins),2)))).flatten()
+    else:
+        sorted_index = np.argsort(np.linalg.norm(coin_xy_rel, axis=1))[:COIN_K]
+        coins_feat = np.sign(coin_xy_rel[sorted_index]).flatten()
+
+    # construct field feature
+    xx, yy = np.clip(
+        np.mgrid[x - SIGHT : x + SIGHT, y - SIGHT : y + SIGHT], 0, arena.shape[0] - 1
+    )
+
+    ##take absolute value so walls and crates are treated equally
+    #rel_obstacle_map = np.abs(arena[xx, yy]).flatten().astype("int32")
+
+    #relative field
+    rel_field = np.array(arena[xx,yy]).flatten().astype("int32")
+
+    ##construct map of coins in SIGHT
+    #rel_coin_map = np.zeros((len(yy), len(xx)))
+    #coin_xy = [[coin_x,coin_y] for (coin_x,coin_y) in coins]
+    #for coin in coin_xy:
+    #    for xc in xx:
+    #        for yc in yy:
+    #            if (xc,yc) == coin:
+    #                rel_coin_map[xc, yc] = 1
+    #rel_coin_map = rel_coin_map.flatten().astype("int32")
+    ##construct map of crates likewise
+    #rel_crate_map = arena[xx, yy]>0
+    #rel_crate_map = rel_crate_map.flatten().astype("int32")
+
+    # construct explosion map feature
+    rel_exp_map = explosion_map[xx, yy].flatten().astype("int32")
+
+    # turn bombs left into int32 numpy array with
+    bombs_left = (np.asarray(bombs_left)).astype("int32")
+
+    # construct feature vector
+    feature_vec = np.concatenate(
+    #    (rel_field, rel_exp_map, np.array([x, y]), bomb_feat, coins_feat)
+        (rel_field, coins_feat)
+    #    (rel_obstacle_map, rel_coin_map, rel_crate_map)
+    )
+    #feature_vec = np.append(feature_vec, score)
+    #feature_vec = np.append(feature_vec, bombs_left)
+
+    return feature_vec
 
 
 def setup(self):
-    self.keep_training = False
-    for arg in sys.argv:
-        if arg=="--keep-training":
-            self.keep_training = True
-            break
-    if self.train:
-        self.model = [[], [], [], [], [], []]
-        self.feat_history = [deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE)]
-        self.next_feat_history = [deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE)]
-        self.reward_history = [deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE), deque(maxlen=HISTORY_SIZE)]
-        self.epsilon = EPSILON_TRAIN
-        self.logger.debug("feat_history", self.feat_history)
-    else:  
-        self.epsilon = 0
 
-    if (self.train and not self.keep_training) or not os.path.isfile("my-saved-model.pt"):
-        X, y = make_regression(n_features=FEAT_DIM)
-        for idx in A_IDX:
-            self.model[idx].fit(X, y)
+    self.betas = [[], [], [], [], [], []]
+    self.feat_history = [[], [], [], [], [], []]
+    self.target_history = [[], [], [], [], [], []]
+
+    #self.betas = [np.ones(58) for _ in enumerate(self.betas)]
+    self.betas = [np.ones(6) for _ in enumerate(self.betas)]
+    self.Q_pred = 1
+
+    if self.train or not os.path.isfile("my-saved-model.pt"):
+        # self.logger.info("Setting up model from scratch.")
+        weights = np.random.rand(len(ACTIONS))
+        self.model = weights / weights.sum()
     else:
-        self.logger.info("Loading model from saved state.")
+        # self.logger.info("Loading model from saved state.")
         with open("my-saved-model.pt", "rb") as file:
             self.model = pickle.load(file)
 
 
-def policy_alt(self):
-    prob = np.exp(self.Q_pred/self.rho)
-    #sometimes probabilities are NaN
-    mask = np.isnan(prob)
-    prob[mask]=0
-
-    prob = prob/np.sum(prob)
-        
-    return np.random.choice(np.arange(0,6,1), p=prob.reshape(6))
-
-
-def policy(self, Qs):
-    r = np.random.random(1)
-    prob = np.ones(6)/6
-    if r < self.epsilon:
-        return np.random.choice(np.arange(0, 6, 1), p=prob)
-    else:
-        return np.argmax(Qs)
-
-def Q(self, s, a):
-    return self.model[A_TO_NUM[a]].predict(s)
-
-def Q_func(self, s):
-    return np.array([Q(self, [s], a) for a in ACTIONS])
-
-
 def act(self, game_state: dict):
+
     feat = state_to_features(game_state)
-    Qs = Q_func(self, feat)
-    a = policy(self, Qs)
-    self.logger.debug(f"action a in act: {ACTIONS[a]}")
+    self.Q_pred = Q_func(self, feat)
+    a = policy(self.Q_pred)
+
+    #self.logger.info(f"action a in act: {a}")
     return ACTIONS[a]
--- bomberman_rl/agent_code/linear_regression_agent/train.py
+++ bomberman_rl/agent_code/linear_regression_agent/train.py
@@ -1,73 +1,57 @@
-from os import stat
+from agent_code.my_agent.callbacks import A_TO_NUM, Q_func
+from collections import namedtuple, deque
 import pickle
 from typing import List
-import numpy as np
 import events as e
-
-# from .event_functions import walked_towards_closest_coin, walked_from_danger, \
-#     drop_bomb_next_to_crate, has_no_escape, reward_from_events
-from .event_functions import (
-    walked_towards_closest_coin,
-    walked_from_danger,
-    drop_bomb_next_to_crate,
-    has_no_escape,
-    reward_from_events,
-    walked_towards_closest_crate
-) 
- 
-from .feature_functions import state_to_features
-from .callbacks import A_TO_NUM, Q, ACTIONS
-
-import csv
-
-# Events
-WALKED_TO_COIN = "WALKED_TO_COIN"
-WALKED_FROM_COIN = "WALKED_FROM_COIN"
-DROP_BOMB_NEXT_TO_CRATE = "DROP_BOMB_NEXT_TO_CRATE"
-WALKED_FROM_DANGER = "WALKED_FROM_DANGER"
-STAYS_IN_DANGER_ZONE = "STAYS_IN_DANGER_ZONE"
-HAS_NO_ESCAPE = "HAS_NO_ESCAPE"
-WALKED_TO_CRATE = "WALKED_TO_CRATE"
-
+from agent_code.my_agent.callbacks import state_to_features
+import numpy as np
 
 # This is only an example!
-# Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
+Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
 
 # Hyper parameters -- DO modify
-TRANSITION_HISTORY_SIZE = 1  # keep only ... last transitions
-# ALPHA = 0.1
-#BATCH_SIZE = 2000
-GAMMA = 0.5 #0.5 seems good
-SAMPLE_PROP = 0.1 #proportion of data each tree is fitted on in percent
+TRANSITION_HISTORY_SIZE = 10  # keep only ... last transitions
+RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
+COIN_K = 1
 
-# for convenience
-A_NUM = 6
-A_IDX = np.arange(0, A_NUM, 1, dtype="int")
+# Events
+PLACEHOLDER_EVENT = "PLACEHOLDER"
+ALPHA = 0.1
+BATCH_SIZE = 10
+L = 1
+
+def TD_target_respones(self, gamma=0.1):
+    transition = self.transitions.pop()
+
+    # Q function of next value
+    # next_feat = state_to_features(transition.next_state)
+    next_q_value = Q_func(self, transition.next_state)
+
+    return transition.reward + gamma * np.max(next_q_value)
+
+
+def gradient_update(self, idx):
+
+    if len(self.feat_history[idx]) >= BATCH_SIZE:
+        selection_size = len(self.target_history[idx])
+        selection_mask = np.random.permutation(np.arange(selection_size))[0:BATCH_SIZE]
+
+        Y_tt = np.array(self.target_history[idx])[selection_mask]
+        feats = np.array(self.feat_history[idx])[selection_mask]
+        RHS = np.sum(np.sum(feats * (Y_tt[:, None] - feats * self.betas[idx]), axis=1))
+        self.betas[idx] = self.betas[idx] + (ALPHA / BATCH_SIZE) * RHS
+
+    else:
+        Y_tt = np.array(self.target_history[idx])
+        feats = np.array(self.feat_history[idx])
+        RHS = np.sum(np.sum(feats * (Y_tt[:, None] - feats * self.betas[idx]), axis=1))
+        self.betas[idx] = self.betas[idx] + (ALPHA / len(self.feat_history[idx])) * RHS
+
+    self.betas=[np.array(beta)/np.linalg.norm(np.array(self.betas), ord=2) for beta in self.betas]
 
 
 def setup_training(self):
-    # self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
-    self.reward_data = 0 # for plotting
-    self.cc = 0
-
-    with open('data.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow(['round', 'score', 'survival time', 'total rewards', 'cc'])
-
-
-def model_update(self):
-    #select random batch of transitions and updates all actions at once
-    for idx in A_IDX:
-        self.logger.debug(f"update action {ACTIONS[idx]}")
-        # self.logger.debug(f"Q_values: {next_q_value}")
-        if len(self.feat_history[idx])>0:
-            X = np.array(self.feat_history[idx])
-            next_q_value = np.array([Q(self, np.array(self.next_feat_history[idx]), a) for a in ACTIONS])
-            # self.model[idx].set_params(max_samples=int(np.sqrt(len(X))))  #only choose 1/10 of dataset for fitting each tree
-            y = np.array(self.reward_history[idx]) + GAMMA * np.max(next_q_value, axis=0)
-            self.model[idx].fit(np.array(X),np.array(y))
-        else:
-            continue
+    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
 
 
 def game_events_occurred(
@@ -78,66 +62,134 @@ def game_events_occurred(
     events: List[str],
 ):
 
-    # custom events use event_functions here
-    if old_game_state is not None:
-        old_feat = state_to_features(old_game_state)
-        new_feat = state_to_features(new_game_state)
-        if walked_towards_closest_coin(old_feat, self_action):
-            events.append(WALKED_TO_COIN)
-        if walked_from_danger(old_feat, self_action) == 1:
-            events.append(WALKED_FROM_DANGER)
-        elif walked_from_danger(old_feat, self_action) == -1:
-            events.append(STAYS_IN_DANGER_ZONE)
-        if drop_bomb_next_to_crate(old_feat, events):
-            events.append(DROP_BOMB_NEXT_TO_CRATE)
-        if has_no_escape(new_feat, self_action):
-            events.append(HAS_NO_ESCAPE)
-        reward = total_rewards(self, events, old_game_state, new_game_state)
-        self.reward_data += reward
-        for event in events:
-            if event == e.COIN_COLLECTED:
-                self.cc += 1
+    # Idea: Add your own events to hand out rewards
+    if ...:
+        events.append(PLACEHOLDER_EVENT)
 
+    # state_to_features is defined in callbacks.py
+    if old_game_state is not None:
+        self.transitions.append(
+            Transition(
+                state_to_features(old_game_state),
+                self_action,
+                state_to_features(new_game_state),
+                #reward_from_events(self, events),
+                total_rewards(self, events, old_game_state, new_game_state)
+            )
+        )
+        feat = state_to_features(old_game_state)
+
+        Y_tt = TD_target_respones(self, 0.1)
         idx = A_TO_NUM[self_action]
-        # print('feat: ', state_to_features(new_game_state))
-        # self.logger.debug(f"feature_vec: {old_feat}")
-        self.feat_history[idx].append(old_feat)
-        self.reward_history[idx].append(reward)
-        self.next_feat_history[idx].append(new_feat)
+
+        self.target_history[A_TO_NUM[self_action]].append(Y_tt)
+        self.feat_history[idx].append(feat)
+        #self.logger.info(f"self.target_history: {self.target_history}")
+        #self.logger.info(f"self.feat_history: {self.feat_history}")
+
+        gradient_update(self, idx)
 
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
-    with open('data.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow((last_game_state["round"], last_game_state["self"][1], last_game_state["step"], self.reward_data, self.cc))
-        
-    reward = total_rewards(self, events, last_game_state, None)
-
-    old_feat = state_to_features(last_game_state)
-    new_feat = state_to_features(None)
-    idx = A_TO_NUM[last_action]
-    # self.logger.debug(f"feature_vec: {old_feat}")
-    self.feat_history[idx].append(old_feat)
-    self.reward_history[idx].append(reward)
-    self.next_feat_history[idx].append(new_feat)
-
-    # reset reward counter for plotting
-    self.reward_data = 0
-    self.cc = 0
-
-    model_update(self)
+    #self.logger.debug(
+    #    f'Encountered event(s) {", ".join(map(repr, events))} in final step'
+    #)
+    self.transitions.append(
+        Transition(
+            state_to_features(last_game_state),
+            last_action,
+            None,
+            reward_from_events(self, events),
+        )
+    )
 
     # Store the model
     with open("my-saved-model.pt", "wb") as file:
-        pickle.dump(self.model, file)
-    # with open("current_model/feature_history.pt", "wb") as file:
-    #     pickle.dump(self.feat_history, file)
-    # with open("current_model/next_feature_history.pt", "wb") as file:
-    #     pickle.dump(self.next_feat_history, file)
-    # with open("current_model/reward_history.pt", "wb") as file:
-    #     pickle.dump(self.reward_history, file)
-
+        pickle.dump(self.betas, file)
 
 def total_rewards(self, events, old_game_state, new_game_state):
     rewards = [reward_from_events(self, events)]
+    #rewards.append(potential_reward(self, old_game_state, new_game_state))
     return sum(rewards)
+
+def reward_from_events(self, events: List[str]):
+    game_rewards = {
+        e.COIN_COLLECTED: 100,
+        e.INVALID_ACTION: -100,
+        e.KILLED_SELF: -100,
+        #e.SURVIVED_ROUND: 100,
+        e.CRATE_DESTROYED: 2,
+        e.BOMB_DROPPED: 10
+        # e.KILLED_OPPONENT: 2,
+        # PLACEHOLDER_EVENT: -0.1,  # idea: the custom event is bad
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    #self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
+
+def potential_reward(self, old_game_state, new_game_state):
+    old_pot = coin_potential(self, old_game_state)+bomb_potential(self, old_game_state)
+    new_pot = coin_potential(self, new_game_state)+bomb_potential(self, old_game_state)
+    return old_pot-new_pot #reward when player gets closer to coin(s)
+
+def coin_potential_func(pos):
+    x,y = pos
+    if (x,y) == (0,0):
+        return 0
+    #return (np.abs(x)**L+np.abs(y)**L)**(1/L)/20
+    return np.linalg.norm(pos, ord=L)**2/10
+
+def coin_potential(self, game_state):
+    _, score, bombs_left, (x, y) = game_state["self"]
+    coins = game_state["coins"]
+    coin_xy_rel = np.array([[coin_x-x,coin_y-y] for (coin_x,coin_y) in coins])
+    if len(coin_xy_rel) == 0:
+        return 0
+    if coin_xy_rel.ndim == 1:
+        return coin_potential_func(coin_xy_rel)
+    if len(coins)<COIN_K:
+        sorted_index = np.argsort(np.linalg.norm(coin_xy_rel, axis=1))
+        coins_feat = np.concatenate((coin_xy_rel[sorted_index], np.zeros((COIN_K-len(coins),2))))
+    else:
+        sorted_index = np.argsort(np.linalg.norm(coin_xy_rel, axis=1))[:COIN_K]
+        coins_feat = coin_xy_rel[sorted_index]
+    pot = [coin_potential_func(pos) for pos in coins_feat]
+    non_zero = np.sum(pot!=0)
+    if non_zero == 0:
+        return 0
+    else:
+        return np.sum(pot)/non_zero
+
+def bomb_range(t):
+    #range of explosion as function of remaining time
+    t_inv = 5-t #goes from 1 to 4
+    b_range=[[0,0]]
+    for i in range(t_inv)[0:]:
+        b_range.append([0,i])
+        b_range.append([0,-i])
+        b_range.append([i,0])
+        b_range.append([-i,0])
+    return b_range
+
+def bomb_potential_func(pos):
+    bomb_x,bomb_y,bomb_t = pos
+    for position in bomb_range(bomb_t):
+        if bomb_x == position[0] and bomb_y == position[1]:
+            return -8*(5-bomb_t) #penalty if player is in range for a bomb
+    else:
+        return 0
+    
+
+def bomb_potential(self, game_state):
+    _, score, bombs_left, (x, y) = game_state["self"]
+    bombs = game_state["bombs"]
+    bomb_xy_rel = [[bomb_x-x, bomb_y-y, bomb_t] for ((bomb_x, bomb_y), bomb_t) in bombs]
+    pot = [bomb_potential_func(pos) for pos in bomb_xy_rel]
+    non_zero = np.sum(pot!=0)
+    if non_zero == 0:
+        return 0
+    else:
+        return np.sum(pot)/non_zero
